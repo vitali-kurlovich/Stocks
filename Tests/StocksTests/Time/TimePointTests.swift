dc5a0982f929f31886c7b76187c55dc3f3a793c3
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-18 22:53:22 +0400

    Update TimePointTests.swift

--- Tests/StocksTests/Time/TimePointTests.swift
+++ Tests/StocksTests/Time/TimePointTests.swift
@@ -30,6 +30,16 @@ struct TimePointTests {
         #expect(new == PriceTick(time: 12, price: .init(ask: 10, bid: 12)))
     }
 
+    @Test("Set candle price")
+    func setCandlePrice() {
+        let time = TimePoint(time: 12)
+
+        let price = CandlePrice(open: 4, close: 5, low: 1, high: 10)
+        let new = time.setPrice(price)
+
+        #expect(new == PriceCandle(time: 12, price: .init(open: 4, close: 5, low: 1, high: 10)))
+    }
+
     @Test("Set ticks volume")
     func setTickVolume() {
         let time = TimePoint(time: 12)
@@ -39,7 +49,15 @@ struct TimePointTests {
 
         #expect(new == VolumeTick(time: 12, volume: .init(ask: 10, bid: 12)))
     }
-    
+
+    @Test("Set candle volume")
+    func setCandleVolume() {
+        let time = TimePoint(time: 12)
+        let new = time.setVolume(12.0)
+
+        #expect(new == VolumeCandle(time: 12, volume: 12.0))
+    }
+
     @Test("Set ticks price & volume")
     func setTickPriceVolume() {
         let time = TimePoint(time: 12)
@@ -51,4 +69,14 @@ struct TimePointTests {
         #expect(new == Tick(time: 12, price: .init(ask: 100, bid: 120), volume: .init(ask: 10, bid: 12)))
     }
 
+    @Test("Set candle price & volume")
+    func setCandlePriceVolume() {
+        let time = TimePoint(time: 12)
+
+        let price = CandlePrice(open: 4, close: 5, low: 1, high: 10)
+
+        let new = time.setPrice(price, volume: 12.0)
+
+        #expect(new == Candle(time: 12, price: .init(open: 4, close: 5, low: 1, high: 10), volume: 12.0))
+    }
 }
