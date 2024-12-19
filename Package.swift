094e96ace933c95ba958eb7838168215d842de12
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-20 00:26:17 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/vitali-kurlovich/TimeSeries", from: "0.2.0"),
+        .package(url: "https://github.com/vitali-kurlovich/TimeSeries", from: "0.3.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +26,7 @@ let package = Package(
         ),
         .testTarget(
             name: "StocksTests",
-            dependencies: ["Stocks"]
+            dependencies: ["Stocks", "TimeSeries"]
         ),
     ]
 )
