988cacc8003373211c73304457a8adf9803c89ca
vitali-kurlovich <vitalikurlovich@gmail.com>
2024-12-19 09:04:25 +0400

    Update PriceCandle.swift

--- Sources/Stocks/Candle/PriceCandle.swift
+++ Sources/Stocks/Candle/PriceCandle.swift
@@ -19,8 +19,23 @@ struct PriceCandle<Time: BaseValue, PriceValue: BaseValue>: Hashable, Sendable {
     }
 }
 
+public
 extension PriceCandle {
-    func setTime(_ time: Time) -> Self {
+    func setTime<TimeValue: BaseValue>(_ time: TimeValue) -> PriceCandle<TimeValue, PriceValue> {
         .init(time: time, price: price)
+    }
+
+    func setPrice<CandlePriceValue: BaseValue>(_ price: CandlePrice<CandlePriceValue>) -> PriceCandle<Time, CandlePriceValue> {
+        .init(time: time, price: price)
+    }
+
+    func setVolume<VolumeValue: BaseValue>(_ volume: VolumeValue) -> Candle<Time, PriceValue, VolumeValue> {
+        .init(time: time, price: price, volume: volume)
+    }
+
+    func setPrice<CandlePriceValue: BaseValue, VolumeValue: BaseValue>(_ price: CandlePrice<CandlePriceValue>,
+                                                                       volume: VolumeValue) -> Candle<Time, CandlePriceValue, VolumeValue>
+    {
+        .init(time: time, price: price, volume: volume)
     }
 }
